cbb7a2244a5e8e52085da667282c66152e7d9bbc
FedoraSmae <71103121+FedoraSmae@users.noreply.github.com>
2020-09-24 19:21:21 +0100

    Add files via upload

--- Game/main.lua
+++ Game/main.lua
@@ -1,75 +1,165 @@
 function love.load()
-    sound = {}
-    sound.grassrun = love.audio.newSource("audio/grassrun.mp3", "stream")
-    sound.grasswalk = love.audio.newSource("audio/grassslow.mp3", "stream")
+
+    --Makes the windows resolution 1080p, standard for most modern day monitors, but is resizable
+
+    window = love.window.setMode( 1920, 1080, {resizable=true, vsync=false, minwidth=400, minheight=300})
+
+    --Loads necessary Libraries
+
+    anim8 = require'libraries/anim8/anim8'
+    sti = require'libraries/Simple-Tiled-Implementation/sti'
+    cameraFile = require'libraries/hump/camera'
+    require'libraries/show'
+    require'libraries/TEsound'
+
+    cam = cameraFile()
+
+    --Saves data for the game, currently saves: Map Position, Player Health
+
+    saveData = {}
+    saveData.px = 360
+    saveData.py = 500
+    saveData.phealth = 100
+
+    --Checks data.lua for existing saved data
+
+    if love.filesystem.getInfo("data.lua") then
+        local data = love.filesystem.load("data.lua")
+        data()
+    end
+
+    --Sprite table, loads sprite files
 
     sprites = {}
-    sprites.background = love.graphics.newImage('sprites/background.png')
-    sprites.flower = love.graphics.newImage('sprites/FlowerFrontx8.png')
-    sprites.flowerb = love.graphics.newImage('sprites/FlowerBackx8.png')
-    sprites.flowerl = love.graphics.newImage('sprites/FlowerLeftx8.png')
-    sprites.flowerR = love.graphics.newImage('sprites/FlowerRightx8.png')
-    sprites.flowerC = love.graphics.newImage('sprites/FlowerCoolx8.png')
-    sprites.bullet = love.graphics.newImage('sprites/bullet.png')
-    sprites.tb = love.graphics.newImage('sprites/TB.png')
-    sprites.main = sprites.flower
+    sprites.playerSheet = love.graphics.newImage('sprites/playerSheet.png')
+    sprites.map = love.graphics.newImage('sprites/test6.png')
 
-    player = {}
-    player.x = love.graphics.getWidth() / 2
-    player.y = love.graphics.getHeight() / 2
-    player.speed = 120
+    local grid = anim8.newGrid(136, 184, sprites.playerSheet:getWidth(), sprites.playerSheet:getHeight())
+
+    --Animations table, loads animation files
+
+    animations = {}
+    animations.sidewalk = anim8.newAnimation(grid('1-4',1), 0.1)
+    animations.backwalk = anim8.newAnimation(grid('1-4',2), 0.1)
+    animations.frontwalk = anim8.newAnimation(grid('1-4',3), 0.1)
+    animations.idle = anim8.newAnimation(grid('1-4',4), 0.5)
+
+    music = {}
+    music.mainmusic1 = love.audio.newSource('music/DieYoung.ogg', 'stream')
+    music.mainmusic2 = love.audio.newSource('music/Tune Up vs Italobrothers - Colours Of The Rainbow.WAV', 'stream')
+
+    --Loads the information for the hitbox world
+
+    wf = require'libraries/windfield/windfield'
+    world = wf.newWorld(0, 0, false)
+
+    world:addCollisionClass('player')
+    world:addCollisionClass('obstacle')
+    world:addCollisionClass('bad')
+
+    --Loads the player.lua file which contains all the player information
+
+    require('player')
+
+    --badthing = world:newRectangleCollider(0, 550, 800, 50, {collision_class = "bad"})
+    --badthing:setType('static')
+
+    --Obstacle table, loads all obstacles on the map, is auto generated
+
+    obstacles = {}
+
+    --Loads the tilemap
+
+    loadMap()
+
+    --Selects a random song from the list and plays it, once its finished, it will randomly select it again.
+
+    TEsound.playLooping({'music/DieYoung.ogg', 'music/Tune Up vs Italobrothers - Colours Of The Rainbow.WAV'}, 'stream')
+
 end
 
 function love.update(dt)
 
-    --Controls player movability
-
-    if love.keyboard.isDown("d") then
-        player.x = player.x + player.speed*dt
-    end
-    if love.keyboard.isDown("a") then
-        player.x = player.x - player.speed*dt
-    end
-    if love.keyboard.isDown("w") then
-        player.y = player.y - player.speed*dt
-    end
-    if love.keyboard.isDown("s") then
-        player.y = player.y + player.speed*dt
-    end
-
-    --Enables sprinting, and controls the audio for walking, and running
-
-    if love.keyboard.isDown("lctrl") and love.keyboard.isDown("d", "a", "w", "s") then
-        player.speed = 240
-        love.audio.play(sound.grassrun)
-    elseif love.keyboard.isDown("d", "a", "w", "s") then
-        player.speed = 120
-        love.audio.stop(sound.grassrun)
-        love.audio.play(sound.grasswalk)
-    else
-        player.speed = 120
-        love.audio.stop(sound.grassrun)
-        love.audio.pause(sound.grasswalk)
-    end
+    --Updates the hitbox map, and the game map
     
+    world:update(dt)
+    gameMap:update(dt)
+    TEsound.cleanup()
+
+    --Calls playerUpdate function found in player.lua
+
+    playerUpdate(dt)
+
+    --Calculates camera position so it stays on the player
+
+    local px, py = player:getPosition()
+    cam:lookAt(px, py)
+
+    --Assigns the value of px, and py to saveData, allowing for the position to be kept next time the game is loaded
+
+    saveData.px = px-47
+    saveData.py = py-65
+    saveData.phealth = player.health
+    love.filesystem.write("data.lua", table.show(saveData, "saveData"))
+
 end
 
 function love.draw()
-    love.graphics.draw(sprites.background, 0, 0)
 
-    love.graphics.draw(sprites.main, player.x, player.y, nil, 0.5, 0.5)
+    --Anything in cam:attach is drawn, but not tied to the screen, anything in cam:detach is tied to the screen
 
-    --Character Sprite rotation when walking
+    cam:attach()
+        gameMap:drawLayer(gameMap.layers["Tile Layer 1"])
+        gameMap:drawLayer(gameMap.layers["Objects"])
+        --world:draw()
+        drawPlayer()
+    cam:detach()
 
-    if love.keyboard.isDown("w") then
-        sprites.main = sprites.flowerb
-    elseif love.keyboard.isDown("a") then
-        sprites.main = sprites.flowerl
-    elseif love.keyboard.isDown("d") then
-        sprites.main = sprites.flowerR
-    elseif love.keyboard.isDown("c") and love.keyboard.isDown("o") then
-        sprites.main = sprites.flowerC
-    else
-        sprites.main = sprites.flower
+    --Displays the co-ordinates and FPS when f3 is held down
+    
+    local px, py = player:getPosition()
+    if love.keyboard.isDown('f3') then
+        love.graphics.print("x ="..px)
+        love.graphics.print("y ="..py, 0, 10)
+        love.graphics.print("FPS:"..tostring(love.timer.getFPS()), 0, 20)
+    end
+
+    if love.keyboard.isDown('m') then
+        love.graphics.draw(sprites.map, 0, 0, nil , 1)
+    end
+
+    --Creates the player's health bar, and colours it red, it drains as the players health decreases.
+
+    love.graphics.push()
+        love.graphics.setColor(255, 0, 0)
+        love.graphics.rectangle("fill", love.graphics.getWidth() - 75, love.graphics.getHeight() - 75, 50, saveData.phealth * -3)
+        love.graphics.setColor(255, 255, 255)
+    love.graphics.pop()
+
+    --[[love.graphics.push()
+        love.graphics.setColor(0, 255, 0)
+        love.graphics.rectangle("fill", love.graphics.getWidth() - 125, love.graphics.getHeight() - 75, 50, player.stamina * -3)
+        love.graphics.setColor(255, 255, 255)
+    love.graphics.pop()]]
+
+
+end
+
+function spawnObstacles(x, y, width, height)
+
+    --Creates obstacles/boundaries based on their position in the TileMap
+
+    local obstacle = world:newRectangleCollider(x, y, width, height, {collision_class = "obstacle"})
+    obstacle:setType('static')
+    table.insert(obstacles, obstacle)
+end
+
+function loadMap()
+
+    --Loads the TileMap, and calculates the positions of the obstacles/boundaries, and assigns it the the spawnObstacles function
+
+    gameMap = sti("map/map.lua")
+    for i, obj in pairs(gameMap.layers["Boundaries"].objects) do
+        spawnObstacles(obj.x, obj.y, obj.width, obj.height)
     end
 end
---
+++ Game/player.lua
@@ -0,0 +1,113 @@
+--This file contains all information, and processes, involved with the Player
+
+--Draws the Player hitbox
+
+player = world:newRectangleCollider(saveData.px, saveData.py, 95.2, 128.8, {collision_class = "player"})
+player:setFixedRotation(true)
+
+--All the player related stats
+
+player.speed = 120
+player.animation = animations.idle
+player.isMovingside = false
+player.isMovingup = false
+player.isMovingDown = false
+player.direction = 1
+player.health = saveData.phealth
+
+--checks of the save data doesn't have a value set to it, if not then the health is set to 100
+
+if saveData.phealth == nil then
+    player.health = 100
+end
+player.stamina = 100
+
+function playerUpdate(dt)
+
+    --Everything to do with the players movement
+
+    if player.body then
+        player.isMovingside = false
+        player.isMovingup = false
+        player.isMovingDown = false
+        local px, py = player:getPosition()
+        if love.keyboard.isDown('d') then
+            player:setX(px + player.speed*dt)
+            player.isMovingside = true
+            player.direction = 1
+        end
+        if love.keyboard.isDown('a') then
+            player:setX(px - player.speed*dt)
+            player.isMovingside = true
+            player.direction = -1
+        end
+        if love.keyboard.isDown('w') then
+            player:setY(py - player.speed*dt)
+            player.isMovingup = true
+        end
+        if love.keyboard.isDown('s') then
+            player:setY(py + player.speed*dt)
+            player.isMovingDown = true
+        end
+
+        if player:enter('bad') then
+            player:destroy()
+        end
+    end
+
+    local run = false
+
+    --Controls the players sprinting, and how fast they can go
+
+    if love.keyboard.isDown('lctrl') and love.keyboard.isDown("d", "a", "w", "s") then
+        player.speed = 240
+        run = true
+    elseif love.keyboard.isDown("d", "a", "w", "s") then
+        player.speed = 120
+        run = false
+    else
+        player.speed = 120
+        run = false
+    end
+
+    --Chooses which animation to display on screen based on which key is pressed and what the player is doing
+
+    if player.isMovingside then
+        player.animation = animations.sidewalk
+    elseif player.isMovingup then
+        player.animation = animations.backwalk
+    elseif player.isMovingDown then
+        player.animation = animations.frontwalk
+    else
+        player.animation = animations.idle
+
+    end
+
+    --Updates the animation so the next frame is displayed on screen
+
+    player.animation:update(dt)
+
+    if love.keyboard.isDown('p') then
+        player.health = player.health - 1
+    end
+
+    --[[while player.stamina <= 0 and player.stamina < 101 and run == false do
+        player.stamina = player.stamina + 1
+    end]]
+
+
+
+    --[[if player.health <= 0 then
+        player:destroy()
+    end]]
+
+end
+
+function drawPlayer()
+
+    --Draws the players sprite in the correct position within the hitbox, as well as where it was last saved
+
+    local px, py = player:getPosition()
+    player.animation:draw(sprites.playerSheet, px, py, nil, 0.7 * player.direction, 0.7, 68, 92)
+    
+end
